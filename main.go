94523ea2c33374284fe1fe933451b2880b2c0171
MJR <m.j.rahman@outlook.com>
2022-02-06 20:10:26 +0000

    test

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+	tracker "tracker/groupie"
+)
+
+func main() {
+	fmt.Printf("Starting server...\n")
+	fmt.Printf("Listening on port 8080!\n")
+	tracker.OpenBrowser("http://localhost:8080")
+	tracker.MainHandler()
+}
