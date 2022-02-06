143a58b301542504dc2c6a30563293d2ff5e0335
MJR <m.j.rahman@outlook.com>
2022-02-06 20:22:43 +0000

    test

--- groupie/structs.go
+++ groupie/structs.go
@@ -1,18 +1,18 @@
 package tracker
 
 type Artists struct {
-	ID             string   `json:"id"`
+	ID             int64    `json:"id"`
 	Image          string   `json:"image"`
 	Name           string   `json:"name"`
 	Members        []string `json:"members"`
-	CreationDate   string   `json:"creationDate"`
+	CreationDate   int64    `json:"creationDate"`
 	FirstAlbum     string   `json:"firstAlbum"`
 	DatesLocations map[string][]string
 }
 
 type Relation struct {
 	Index []struct {
-		ID             string              `json:"id"`
+		ID             int64               `json:"id"`
 		DatesLocations map[string][]string `json:"datesLocations"`
 	} `json:"index"`
 }
